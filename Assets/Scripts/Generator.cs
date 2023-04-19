fd0468eacedc94c281aeefd73ce90858ef439055
MichailS-000 <sobolev.miha2014@yandex.ru>
2023-04-20 01:38:22 +0300

    Night commit

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,6 +7,7 @@ public class GeneratorSegment
 {
 	public GameObject segmentObject;
 	public Vector3 postGenerationAnchor;
+	public Vector3 preGenerationAnchor;
 
 	public int[] possibleSegments;
 	public int[] possibleRotationOffsets;
@@ -20,20 +21,19 @@ public class Generator : MonoBehaviour
 	[SerializeField] GeneratorSegment[] segments;
 
 	GeneratorSegment lastGenerated;
-	Vector3 lastGeneratedPosition = Vector3.down;
+	Vector3 lastGeneratedPosition = Vector3.zero;
 	int lastGeneratedRotation = 0;
 
 	int seed;
 
 	void CreateSegment(int segmentId)
 	{
-		lastGeneratedPosition = lastGeneratedPosition + segments[segmentId].postGenerationAnchor;
-		lastGeneratedRotation = segments[segmentId].possibleRotationOffsets[Random.Range(0, segments[segmentId].possibleRotationOffsets.Length)] + lastGeneratedRotation;
+		lastGeneratedPosition = lastGeneratedPosition + lastGenerated.postGenerationAnchor + segments[segmentId].preGenerationAnchor;
 
 		Instantiate(segments[segmentId].segmentObject,
 			lastGeneratedPosition,
 			Quaternion.Euler(0,
-			lastGeneratedRotation, 0));
+			0, 0));
 
 		lastGenerated = segments[segmentId];
 	}
@@ -49,9 +49,11 @@ public class Generator : MonoBehaviour
 
 		PhotonNetwork.Instantiate(player.name, random.position, random.rotation);
 
+		lastGenerated = segments[0];
+
 		CreateSegment(1);
 
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 20; i++)
 		{
 			CreateSegment(lastGenerated.possibleSegments[Random.Range(0, lastGenerated.possibleSegments.Length)]);
 		}
